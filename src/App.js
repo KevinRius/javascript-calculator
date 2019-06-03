afa4289fd2fb35294df6ffc652be74f0c4b42cb9
KevinRius <k.riou29@gmail.com>
2019-06-03 18:20:40 +0900

    calculator layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Row, Button} from 'reactstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App vh-100">
+      <div className="row h-100">
+        <div className="col-12 align-self-center">
+          <div className="text-right">0</div>
+          <Row className="pad">
+            <Button color="danger" className="col-6">AC</Button>
+            <Button color="info" className="col-3">/</Button>
+            <Button color="info" className="col-3">*</Button>
+            <Button color="primary" className="col-3 ">7</Button>
+            <Button color="primary" className="col-3">8</Button>
+            <Button color="primary" className="col-3">9</Button>
+            <Button color="info" className="col-3">-</Button>
+            <Button color="primary" className="col-3">4</Button>
+            <Button color="primary" className="col-3">5</Button>
+            <Button color="primary" className="col-3">6</Button>
+            <Button color="info" className="col-3">+</Button>
+            <div className="row w-75 m-0">
+              <Button color="primary" className="col-4">1</Button>
+              <Button color="primary" className="col-4">2</Button>
+              <Button color="primary" className="col-4">3</Button>
+              <Button color="primary" className="col-8">0</Button>
+              <Button color="primary" className="col-4">.</Button>
+            </div>
+            <Button color="success" className="col-3">=</Button>
+          </Row>
+        </div>
+      </div>
+    </Container>
   );
 }
 
